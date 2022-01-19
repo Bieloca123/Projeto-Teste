c389fccbe7ab25721e496271e8fcc64a33ce324e
Bieloca123 <gabrielfreitasdfc123@icloud.com>
2022-01-19 15:02:25 -0200

    [API]

--- produtos/config/application.rb
+++ produtos/config/application.rb
@@ -10,7 +10,9 @@ module Produtos
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.api_only = true
+    config.debug_exception_response_format = :default
+    config.debug_exception_response_format = :api
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
